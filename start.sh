11d828c49ae895014ca63e4c70531290a8828adf
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-30 16:23:07 +0200

    Add build script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --privileged --net=host -e DISPLAY -v /dev/snd:/dev/snd -v /tmp/.X11-unix:/tmp/.X11-unix:rw  hoeghh/mame /usr/games/mame
+docker run --privileged --net=host -e DISPLAY -v /dev/snd:/dev/snd -v /tmp/.X11-unix:/tmp/.X11-unix:rw  praqma/dame /usr/games/mame
