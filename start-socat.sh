cc2c9a140bf6f5827164ab4a2ba5a756ab5f6170
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-31 20:23:51 +0200

    Fix issue #2

--- start-socat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## Used on Mac OSX
-socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\"
-
--- start.sh
+++ start.sh
@@ -10,22 +10,30 @@ then
              -v /dev/snd:/dev/snd \
              -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
              praqma/dame /usr/games/mame
+
 elif [ "$kernel_name" == "Darwin" ]
 then
   # Mac OSX
-  ## Remember to start socat first in another terminal
 
-  ## Start XQuartz to show windows
+  ## Enable X11 listening by exposing a local XQuartz socket on TCP port 6000
+  (socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\") &
+
+  ## Start XQuartz to show X11 windows
   open -a XQuartz
 
   ## On OSX you need to set the DISPLAY variable to the IP of your vboxnet0 network interface
   vboxnet0_ip=$(ifconfig vboxnet0 | grep -Eo 'inet [0-9]+.[0-9]+.[0-9]+.[0-9]+' | cut -d " " -f 2)
   docker run --privileged \
+             --rm \
              --net=host \
              -e DISPLAY=$vboxnet0_ip:0 \
-             -v /dev/snd:/dev/snd \
              -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
              praqma/dame /usr/games/mame
+
+  osascript -e 'quit app "XQuartz"'
+  ## When docker finishes, stop socat by killing the last background process
+  kill $!
+
 else
   echo OS not supported: $kernel_name
 fi
